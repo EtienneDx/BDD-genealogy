2bd378e0b9efade95829c8a86207eb8c09da1a18
FANNIE YAN <81410437+fannieyan@users.noreply.github.com>
2023-04-06 14:28:01 +0200

    Inject database service in app

--- packages/back-end/src/index.ts
+++ packages/back-end/src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import DatabaseServiceImpl from './entities/database';
+import neo4j from 'neo4j-driver';
 
 export default function createApp() {
   const app = express();
+  app.set(
+    'databaseService',
+    new DatabaseServiceImpl(neo4j.driver('bolt://localhost:7687'))
+  );
 
   app.get('/', (_, res) => {
     res.json({ message: 'Hello World' });
