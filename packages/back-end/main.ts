2aebefa6e1ecc63b6f354b436ec0c5374a766f1d
FANNIE YAN <81410437+fannieyan@users.noreply.github.com>
2023-04-06 16:30:08 +0200

    Extract database Service dependency

--- packages/back-end/main.ts
+++ packages/back-end/main.ts
@@ -1,6 +1,11 @@
 import createApp from './src';
+import DatabaseServiceImpl from './src/entities/database';
+import neo4j from 'neo4j-driver';
 
-const app = createApp();
+const databaseService = new DatabaseServiceImpl(
+  neo4j.driver('bolt://localhost:7687')
+);
+const app = createApp(databaseService);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
